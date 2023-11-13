42f9f2ee0f618c7d01a731b60e5d994e6375cd1b
An0nimia <41164945+An0nimia@users.noreply.github.com>
2023-11-13 21:21:10 +0100

    Adding saiyan option for combine media
    Some album | playlist are very big so cannot ask to media all tracks that you want

--- api_deezer_full/media/api.py
+++ api_deezer_full/media/api.py
@@ -10,9 +10,11 @@ from .exceptions import Insufficient_Rights
 
 class API_Media:
 	__API_MEDIA_URL = 'https://media.deezer.com/v1/get_url'
+	__DEFAULT_MAX_X_MEDIA = 250
+
 
 	@classmethod
-	def get_medias_json(
+	def __get_medias(
 		cls,
 		license_token: str,
 		media_formats: Media_Formats,
@@ -21,18 +23,59 @@ class API_Media:
 
 		json_data = {
 			'license_token': license_token,
-			'media': [
-				media
-				for media in media_formats
-			],
+			'media': media_formats,
 			'track_tokens': track_tokens
 		}
-
 
 		res: dict[str, Any] = req_post(
 			url = cls.__API_MEDIA_URL,
 			json = json_data
 		).json()
+
+		return res
+
+
+	@classmethod
+	def __get_medias_all(
+		cls,
+		license_token: str,
+		tracks_token: list[str],
+		media_formats: Media_Formats,
+		n_tracks: int,
+		medias: dict[str, Any]
+	) -> Medias:
+
+		for a in range(0, n_tracks, cls.__DEFAULT_MAX_X_MEDIA):
+			medias += cls.__get_medias(
+				license_token = license_token,
+				media_formats = media_formats[a:a + cls.__DEFAULT_MAX_X_MEDIA],
+				track_tokens = tracks_token[a:a + cls.__DEFAULT_MAX_X_MEDIA]
+			)['data']		
+
+		return medias
+
+
+	@classmethod
+	def get_medias_json(
+		cls,
+		license_token: str,
+		media_formats: Media_Formats,
+		track_tokens: list[str],
+		saiyan: bool = True
+	) -> dict[str, Any]:
+
+		n_tracks = len(track_tokens)
+
+		if n_tracks <= cls.__DEFAULT_MAX_X_MEDIA:
+			i = n_tracks
+		else:
+			i = cls.__DEFAULT_MAX_X_MEDIA
+
+		res = cls.__get_medias(
+			license_token = license_token,
+			media_formats = media_formats[:i],
+			track_tokens = track_tokens[:i]
+		)
 
 		errors = res.get('errors')
 
@@ -44,6 +87,15 @@ class API_Media:
 					license_token = license_token
 				)
 
+		if saiyan and n_tracks > cls.__DEFAULT_MAX_X_MEDIA:
+			cls.__get_medias_all(
+				license_token = license_token,
+				tracks_token = track_tokens[cls.__DEFAULT_MAX_X_MEDIA:],
+				media_formats = media_formats[cls.__DEFAULT_MAX_X_MEDIA:],
+				n_tracks = (n_tracks - cls.__DEFAULT_MAX_X_MEDIA),
+				medias = res['data']
+			)
+
 		return res
 
 
@@ -52,11 +104,15 @@ class API_Media:
 		cls,
 		license_token: str, 
 		media_formats: Media_Formats,
-		track_tokens: list[str]
+		track_tokens: list[str],
+		saiyan: bool = True
 	) -> Medias:
 
 		res = cls.get_medias_json(
-			license_token, media_formats, track_tokens
+			license_token = license_token,
+			media_formats = media_formats,
+			track_tokens = track_tokens,
+			saiyan = saiyan
 		)
 
 		return Medias.model_validate(res)
