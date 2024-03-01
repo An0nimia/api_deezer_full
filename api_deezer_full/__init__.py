b9722d8dba90cd686f9512afef7de634f5393ae2
An0nimia <41164945+An0nimia@users.noreply.github.com>
2024-03-01 18:40:03 +0100

    Version 0.0.1b34

--- api_deezer_full/__init__.py
+++ api_deezer_full/__init__.py
@@ -1,10 +1,12 @@
 from .gw import API_GW
 from .pipe import API_PIPE
 from .media import API_Media
+from .mobile import API_Mobile
 
 
 __all__ = (
-	'API_GW', 'API_PIPE', 'API_Media'
+	'API_GW', 'API_PIPE',
+	'API_Media', 'API_Mobile'
 )
 
 
